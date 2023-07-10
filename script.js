d370032ac2132b51de07621f2a844e82194e2bfd
Livni <llmc0383@gmail.com>
2023-07-10 08:37:41 -0600

    se añadio color aleatorio al boton

--- script.js
+++ script.js
@@ -5,37 +5,53 @@
 
 //Se coloca un llamado al boton
 const button = document.querySelector('button')
-const div = document.querySelector('div')
+// const div = document.querySelector('div')
 
-div.style.width= '30px'
-div.style.height= '30px'
-div.style.backgroundColor= 'yellow'
+// div.style.width= '30px'
+// div.style.height= '30px'
+// div.style.backgroundColor= 'yellow'
 
 
 
-//Con esta funcion se retorna
-const functionClic = ()=> {
-    // alert ("hola mundo")
-    console.log('Hicieron un click sobre mi') 
+// //Con esta funcion se retorna
+// const functionClic = ()=> {
+//     // alert ("hola mundo")
+//     console.log('Hicieron un click sobre mi') 
+// }
+
+// //Con esta funcion se entro
+// const functionEnter = ()=> {
+//     console.log('El mouse entro') 
+// }
+
+// //Con esta funcion salio
+// const functionSalir = ()=> {
+//     console.log('El mouse salio') 
+// }
+
+// button.onclick = functionClic
+
+// // const functioClic = ()=> "hola mundo"
+
+
+// //funcion que se asocia a ese evento, se asocia la funcion del click
+// button.addEventListener('click', functionClic)
+// button.addEventListener('mouseenter', functionClic)
+// button.addEventListener('mouseleave', functionClic)
+// div.addEventListener('mouseenter', functionClic)
+
+//se ejecuta la funcion cuando se cargue la imagen
+
+// document.addEventListener('DOM.ContentLoaded' , () =>{
+
+// })
+
+
+//se procede a colocar un color aleatorio al boton
+const randomColor = (number) =>{
+    return Math.floor (Math.random) () * number + 1
 }
 
-//Con esta funcion se entro
-const functionEnter = ()=> {
-    console.log('El mouse entro') 
-}
-
-//Con esta funcion salio
-const functionSalir = ()=> {
-    console.log('El mouse salio') 
-}
-
-button.onclick = functionClic
-
-// const functioClic = ()=> "hola mundo"
-
-
-//funcion que se asocia a ese evento, se asocia la funcion del click
-button.addEventListener('click', functionClic)
-button.addEventListener('mouseenter', functionClic)
-button.addEventListener('mouseleave', functionClic)
-div.addEventListener('mouseenter', functionClic)
+button.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'rgb (${randomColor(255)}, ${randomColor (255)},${randomColor (255)} )'
+})
